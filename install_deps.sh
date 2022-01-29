49ba4b1ab03e768ebf0b001107392757fe9e4ccf
Arp-G <arpanghoshal3@gmail.com>
2022-01-29 23:22:18 +0530

    add script

---
+++ install_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+apt-get update  # To get the latest package lists
+apt-get install transmission-daemon -y # Install transmission torrent client
+apt-get install zip -y # Install transmission torrent client
